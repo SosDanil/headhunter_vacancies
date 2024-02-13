a67ea69ef84688e5cebccaadd7a6d3dbd3d324bc
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 19:10:58 +0500

    add class Vacancy with init

---
+++ src/Vacancy.py
@@ -0,0 +1,8 @@
+class Vacancy:
+    def __init__(self, name, salary_from, salary_to, url, city):
+        self.name = name
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.url = url
+        self.city = city
+
