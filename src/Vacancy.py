f08039bf83acb9d811c465bd28e1e3c3906c5d0a
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 23:06:42 +0500

    add __lt__ to Vacancy + fix __str__ and classmethod

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -14,31 +14,53 @@ class Vacancy:
                 f"{self.url})")
 
     def __str__(self):
-        if self.salary_from is None:
+        if self.salary_from == 0 and self.salary_to == 0:
+            return (f"Профессия: {self.name}, зарплата не указана, "
+                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+        elif self.salary_from == 0:
             return (f"Профессия: {self.name} с зарплатой до {self.salary_to} "
                     f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
-        elif self.salary_to is None:
+        elif self.salary_to == 0:
             return (f"Профессия: {self.name} с зарплатой от {self.salary_from} "
-                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
-        elif self.salary_from == 0 and self.salary_to == 0:
-            return (f"Профессия: {self.name}, зарплата не указана, "
                     f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
         else:
             return (f"Профессия: {self.name} с зарплатой от {self.salary_from} до {self.salary_to} "
                     f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
 
+    def __lt__(self, other):
+        if self.salary_from == 0 and other.salary_from == 0:
+            if self.salary_to < other.salary_to:
+                return True
+            else:
+                return False
+        elif self.salary_to == 0 and other.salary_to == 0:
+            if self.salary_from < other.salary_from:
+                return True
+            else:
+                return False
+        elif self.salary_from != 0 and other.salary_from == 0:
+            return False
+        elif self.salary_from == 0 and other.salary_from != 0:
+            return True
+        elif self.salary_from != 0 and other.salary_from != 0:
+            if self.salary_from < other.salary_from:
+                return True
+            elif self.salary_from == other.salary_from:
+                if self.salary_to < other.salary_to:
+                    return True
+
     @classmethod
     def cast_to_object_list(cls, hh_vacancies):
         for vacancy in hh_vacancies:
-            name = vacancy.get("name")
+            name = vacancy["name"]
             city = vacancy["area"]["name"]
-            url = vacancy.get("alternate_url")
+            url = vacancy["alternate_url"]
             try:
-                salary_from = vacancy["salary"]["from"]
+                salary_from = int(vacancy["salary"]["from"])
             except TypeError:
                 salary_from = 0
             try:
-                salary_to = vacancy["salary"]["to"]
+                salary_to = int(vacancy["salary"]["to"])
             except TypeError:
                 salary_to = 0
             vacancy_to_list = cls(name, city, url, salary_from, salary_to)
