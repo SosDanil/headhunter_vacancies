74523d8fd1c4569b401977cf52f64d7cbfce5cc3
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 19:39:03 +0500

    add str to Vacancy + validation to salary

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,5 +1,6 @@
 class Vacancy:
-    def __init__(self, name: str, salary_from: int, salary_to: int, city: str, url: str):
+    def __init__(self, name: str, city: str, url: str, salary_from: int = "'не указано'",
+                 salary_to: int = "'не указано'"):
         self.name = name
         self.salary_from = salary_from
         self.salary_to = salary_to
@@ -7,10 +8,15 @@ class Vacancy:
         self.url = url
 
     def __repr__(self):
-        return (f"{self.__class__.__name__}({self.name}, {self.salary_from}, {self.salary_to}, {self.city},"
-                f" {self.url})")
+        return (f"{self.__class__.__name__}({self.name}, {self.salary_from}, {self.salary_to}, {self.city}, "
+                f"{self.url})")
+
+    def __str__(self):
+        return (f"Профессия: {self.name} с зарплатой от {self.salary_from} до {self.salary_to} "
+                f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
 
 
-if __name__ == '__main__':
-    test_vacancy = Vacancy('farmer', 30000, 50000, "Perm", "www.ru")
-    print(repr(test_vacancy))
+# if __name__ == '__main__':
+#     test_vacancy = Vacancy("farmer", "Perm", "www.ru")
+#     print(repr(test_vacancy))
+#     print(test_vacancy)
