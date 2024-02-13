70a8cf8266c4903f5db4e5a76afa3d276e98b6b0
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 19:23:10 +0500

    add repr to Vacancy

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,8 +1,16 @@
 class Vacancy:
-    def __init__(self, name, salary_from, salary_to, url, city):
+    def __init__(self, name: str, salary_from: int, salary_to: int, city: str, url: str):
         self.name = name
         self.salary_from = salary_from
         self.salary_to = salary_to
-        self.url = url
         self.city = city
+        self.url = url
 
+    def __repr__(self):
+        return (f"{self.__class__.__name__}({self.name}, {self.salary_from}, {self.salary_to}, {self.city},"
+                f" {self.url})")
+
+
+if __name__ == '__main__':
+    test_vacancy = Vacancy('farmer', 30000, 50000, "Perm", "www.ru")
+    print(repr(test_vacancy))
