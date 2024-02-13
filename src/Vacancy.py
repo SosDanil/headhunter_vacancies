f29d2334c2ff103a879d4857becfab11c542a0e9
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 21:39:59 +0500

    add classmethod cast_to_object_list

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,6 +1,8 @@
 class Vacancy:
-    def __init__(self, name: str, city: str, url: str, salary_from: int = "'не указано'",
-                 salary_to: int = "'не указано'"):
+    vacancies_list = []
+
+    def __init__(self, name: str, city: str, url: str, salary_from: int = 0,
+                 salary_to: int = 0):
         self.name = name
         self.salary_from = salary_from
         self.salary_to = salary_to
@@ -14,6 +16,18 @@ class Vacancy:
     def __str__(self):
         return (f"Профессия: {self.name} с зарплатой от {self.salary_from} до {self.salary_to} "
                 f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+
+    @classmethod
+    def cast_to_object_list(cls, hh_vacancies):
+        for vacancy in hh_vacancies:
+            name = vacancy["name"]
+            city = vacancy["area"]["name"]
+            url = vacancy["alternate_url"]
+            salary_from = vacancy["salary"]["from"]
+            salary_to = vacancy["salary"]["to"]
+            vacancy_to_list = cls(name, city, url, salary_from, salary_to)
+            cls.vacancies_list.append(vacancy_to_list)
+
 
 
 # if __name__ == '__main__':
