4d041a09eb189fa278c00498dea123b8ef981860
Danil Klimenko <sos.danil@yandex.ru>
2024-02-13 18:59:47 +0500

    complete HHAPI

--- src/API.py
+++ src/API.py
@@ -1,16 +1,33 @@
 from abc import ABC, abstractmethod
 
+import requests
+
 
 class BaseAPI(ABC):
     @abstractmethod
-    def get_response(self):
+    def get_response(self, vacancy_name):
         pass
 
 
 class HeadHunterAPI(BaseAPI):
-    def __init__(self):
-        pass
 
-    def get_response(self):
-        pass
+    def get_response(self, vacancy_name):
+        params = {
+            "text": f"NAME:{vacancy_name}",
+            "page": 0,
+            "per_page": 100
+        }
+        response = requests.get(f"https://api.hh.ru/vacancies/", params=params)
+        return response
+
+
+# if __name__ == '__main__':
+#     hh_api = HeadHunterAPI()
+#     hh_response = hh_api.get_response("парикмахер")
+#     print(hh_response)
+#     print(hh_response.status_code)
+#     print(hh_response.ok)
+#     print(hh_response.json())
+
+
 
