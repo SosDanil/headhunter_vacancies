438df87196509edd1e4e82662bb0bb4c630c9026
Danil Klimenko <sos.danil@yandex.ru>
2024-02-15 23:13:55 +0500

    add class JSONManager and write_to_file func

--- src/FileManager.py
+++ src/FileManager.py
@@ -1,15 +1,37 @@
+import json
 from abc import ABC, abstractmethod
 
 
 class FileManager(ABC):
     @abstractmethod
-    def write_to_file(self):
+    def write_to_file(self, path, vacancies):
         pass
 
     @abstractmethod
-    def read_file(self):
+    def read_file(self, path):
         pass
 
     @abstractmethod
-    def delete_from_file(self):
+    def delete_from_file(self, path):
+        pass
+
+
+class JSONManager(FileManager):
+    def __init__(self):
+        pass
+
+    def write_to_file(self, path, vacancies):
+        vacancies_list = []
+        for vacancy in vacancies:
+            json_vacancy = {"Название вакансии": vacancy.name, "Город": vacancy.city, "Ссылка": vacancy.url,
+                            "Зарплата от": vacancy.salary_from, "Зарплата до": vacancy.salary_to,
+                            "Требования": vacancy.requirement, "Обязанности": vacancy.responsibility}
+            vacancies_list.append(json_vacancy)
+        with open(path, 'w', encoding="UTF-8") as f:
+            json.dump(vacancies_list, f, indent=4)
+
+    def read_file(self, path):
+        pass
+
+    def delete_from_file(self, path):
         pass
