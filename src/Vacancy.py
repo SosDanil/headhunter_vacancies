9daccc2ee884034ed4b6b16e593cad247c82e340
Danil Klimenko <sos.danil@yandex.ru>
2024-02-15 17:14:53 +0500

    add requirement and responsibility to Vacancy class + add them to __str__ and fucn cast_to_

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -2,12 +2,14 @@ class Vacancy:
     vacancies_list = []
 
     def __init__(self, name: str, city: str, url: str, salary_from: int,
-                 salary_to: int):
+                 salary_to: int, requirement: str, responsibility: str):
         self.name = name
         self.salary_from = salary_from
         self.salary_to = salary_to
         self.city = city
         self.url = url
+        self.requirement = requirement
+        self.responsibility = responsibility
 
     def __repr__(self):
         return (f"{self.__class__.__name__}({self.name}, {self.salary_from}, {self.salary_to}, {self.city}, "
@@ -16,16 +18,24 @@ class Vacancy:
     def __str__(self):
         if self.salary_from == 0 and self.salary_to == 0:
             return (f"Профессия: {self.name}, зарплата не указана, "
-                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}\n"
+                    f"Требования: {self.requirement}\n"
+                    f"Обязанности: {self.responsibility}\n")
         elif self.salary_from == 0:
             return (f"Профессия: {self.name} с зарплатой до {self.salary_to} "
-                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}\n"
+                    f"Требования: {self.requirement}\n"
+                    f"Обязанности: {self.responsibility}\n")
         elif self.salary_to == 0:
             return (f"Профессия: {self.name} с зарплатой от {self.salary_from} "
-                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}\n"
+                    f"Требования: {self.requirement}\n"
+                    f"Обязанности: {self.responsibility}\n")
         else:
             return (f"Профессия: {self.name} с зарплатой от {self.salary_from} до {self.salary_to} "
-                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}")
+                    f"в городе {self.city}, ссылка на сайт hh.ru: {self.url}\n"
+                    f"Требования: {self.requirement}\n"
+                    f"Обязанности: {self.responsibility}\n")
 
     def __lt__(self, other):
         if self.salary_from == 0 and other.salary_from == 0:
@@ -63,7 +73,16 @@ class Vacancy:
                 salary_to = int(vacancy["salary"]["to"])
             except TypeError:
                 salary_to = 0
-            vacancy_to_list = cls(name, city, url, salary_from, salary_to)
+            try:
+                requirement = vacancy["snippet"]["requirement"]
+            except TypeError:
+                requirement = "не указано"
+            try:
+                responsibility = vacancy["snippet"]["responsibility"]
+            except TypeError:
+                responsibility = "не указано"
+
+            vacancy_to_list = cls(name, city, url, salary_from, salary_to, requirement, responsibility)
             cls.vacancies_list.append(vacancy_to_list)
 
 
