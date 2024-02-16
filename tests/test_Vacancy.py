aee267a328ab321dbd37b2ec5c8440540f16eb67
Danil Klimenko <sos.danil@yandex.ru>
2024-02-16 16:39:15 +0500

    add test_get_filtered

---
+++ tests/test_Vacancy.py
@@ -0,0 +1,67 @@
+import pytest
+from src.Vacancy import Vacancy
+
+test_vacancy1 = Vacancy("Механик",
+                        "Пермь",
+                        "www.hh.ru/vacancies/234561",
+                        "Опыт 1-3 года, ответственность, скиллы мастера,",
+                        "Чинить сломанное, уважать клиентов",
+                        50000, 100000)
+
+test_vacancy2 = Vacancy("Стилист",
+                        "Москва",
+                        "www.hh.ru/vacancies/234562",
+                        "Без опыта, ответственность, свои инструменты",
+                        "Делать людей красивыми",
+                        salary_to=150000)
+
+test_vacancy3 = Vacancy("Бортпроводник",
+                        "Екатеринбург",
+                        "www.hh.ru/vacancies/234563",
+                        "отсутствие страха полетов",
+                        "Разность еду, поддерживать пассажиров",
+                        70000, 170000)
+
+test_vacancy4 = Vacancy("Повар в столовой",
+                        "Пермь",
+                        "www.hh.ru/vacancies/234564",
+                        "Опыт работы в школьной столовой,"
+                        "опрятность, соблюдение гигиены, скиллы мастера",
+                        "Делать людям вкусно и приятно на душе",
+                        20000)
+
+test_vacancy5 = Vacancy("Дворник",
+                        "Екатеринбург",
+                        "www.hh.ru/vacancies/234565",
+                        "без опыта, иметь свою метлу пунктуальность",
+                        "Мастерски подметать дворы")
+
+test_vacancy6 = Vacancy("Лаборант",
+                        "Москва",
+                        "www.hh.ru/vacancies/234567",
+                        "Высшее образование, ответственность, тяга к науке",
+                        "помогать в выполнении научных задач основному персоналу",
+                        20000, 30000)
+
+
+@pytest.fixture
+def vacancies_list():
+    vacancies_list = [test_vacancy1, test_vacancy2, test_vacancy3, test_vacancy4, test_vacancy5, test_vacancy6]
+    return vacancies_list
+
+
+def test_get_filtered_vacancies(vacancies_list):
+    # слова для фильтрации в основной программе будут приходить в малом регистре
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["опыт"]) == [test_vacancy1, test_vacancy2,
+                                                                        test_vacancy4, test_vacancy5]
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["пермь"]) == [test_vacancy1, test_vacancy4]
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["пермь", "дворник"]) == [test_vacancy1,
+                                                                                    test_vacancy4, test_vacancy5]
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["ответственность"]) == [test_vacancy1,
+                                                                                   test_vacancy2, test_vacancy6]
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["механик", "стилист", "повар", "дворник",
+                                                           "лаборант", "проводник"]) == [test_vacancy1, test_vacancy2,
+                                                                                         test_vacancy3, test_vacancy4,
+                                                                                         test_vacancy5, test_vacancy6]
+    assert Vacancy.get_filtered_vacancies(vacancies_list, ["котик"]) == []
+
