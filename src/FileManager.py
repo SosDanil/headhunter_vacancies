67e49ad778f01bb16aa390dd0883d5867ba0b59d
Danil Klimenko <sos.danil@yandex.ru>
2024-02-16 18:54:09 +0500

    add README description

--- src/FileManager.py
+++ src/FileManager.py
@@ -21,6 +21,7 @@ class JSONManager(FileManager):
         pass
 
     def add_to_file(self, path, vacancies):
+        """Записывает данные о вакансиях в файл в формате json"""
         vacancies_list = []
         for vacancy in vacancies:
             json_vacancy = {"Название вакансии": vacancy.name, "Город": vacancy.city, "Ссылка": vacancy.url,
@@ -31,11 +32,13 @@ class JSONManager(FileManager):
             json.dump(vacancies_list, f, indent=4, ensure_ascii=False)
 
     def read_file(self, path):
+        """Выводит информацию из файла"""
         with open(path, "r", encoding="UTF-8") as f:
             data = json.load(f)
         print(json.dumps(data, indent=4,  ensure_ascii=False))
 
     def delete_from_file(self, path):
+        """Очищает файл и оставляет там пустой список"""
         empty_list = []
         with open(path, "w", encoding="UTF-8") as f:
             json.dump(empty_list, f)
